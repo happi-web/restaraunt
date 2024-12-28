310bc8b9fa1861e3dd5aa7d93cc92142c6665ce4
Chilongo Kondwani <chilongokon@gmail.com>
2024-12-28 12:29:32 +0200

    third commit

--- src/modules/about.js
+++ src/modules/about.js
@@ -1,15 +1,10 @@
-const home = document.querySelector("#home");
+const about = document.querySelector("#about");
 
 export function aboutPage() {
     const heading = document.createElement("h1");
-    heading.textContent = "Welcome to Our Restaurant About Us";
+    heading.textContent = "Welcome to Our Restaurant About";
     heading.setAttribute("style", "color: white;")
-
-    if (about) {
-        about.addEventListener("click", () => {
-            const content = document.querySelector("#content");
-            content.innerHTML = ""; // Clear previous content
-            content.appendChild(heading);
-        });
-    }
+    const content = document.querySelector("#content");
+    content.innerHTML = ""; // Clear previous content
+    content.appendChild(heading);
 }
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,15 +1,10 @@
-const home = document.querySelector("#home");
+const menu = document.querySelector("#menu");
 
 export function menuPage() {
     const heading = document.createElement("h1");
     heading.textContent = "Welcome to Our Restaurant Menu";
     heading.setAttribute("style", "color: white;")
-
-    if (menu) {
-        menu.addEventListener("click", () => {
-            const content = document.querySelector("#content");
-            content.innerHTML = ""; // Clear previous content
-            content.appendChild(heading);
-        });
-    }
+    const content = document.querySelector("#content");
+    content.innerHTML = ""; // Clear previous content
+    content.appendChild(heading);
 }
